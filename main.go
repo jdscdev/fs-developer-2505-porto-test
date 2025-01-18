77f7024ba840e539d1eb5ce98a249d63b22e9b75
Joao Coelho <joaodscoelho@gmail.com>
2025-01-18 23:47:20 +0000

    Sorts repositories by ActivityScore and draws TOP10 table

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 
 	repo "github.com/jdscdev/fs-developer-2505-porto-test/internal/models"
 )
@@ -12,11 +13,24 @@ func main() {
 		fmt.Println("Error:", err)
 		return
 	}
-	// repo.GetRepositoriesFromCSVFile("commits.csv")
 
-	// fmt.Println(repositories)
+	// Sort activity score for each repository
+	sort.Slice(repositories, func(i, j int) bool {
+		return repositories[i].ActivityScore > repositories[j].ActivityScore
+	})
 
-	for _, repository := range repositories {
-		fmt.Println("Repo:", repository.RepoName, "Commits:", len(repository.Commits), "ActivityScore:", repository.ActivityScore)
+	fmt.Println(" ********** Top 10 repositories by activity score ********** \n",
+		"-------------------------------------------------------------------------------- \n",
+		" Repo   | #Commits | Files Changed | Lines Added | Lines Deleted | ActivityScore \n",
+		"--------------------------------------------------------------------------------")
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%s | %d       | %d          | %d      | %d       | %.2f\n",
+			repositories[i].RepoName,
+			len(repositories[i].Commits),
+			repositories[i].FilesChanged,
+			repositories[i].LinesAdded,
+			repositories[i].LinesDeleted,
+			repositories[i].ActivityScore)
 	}
 }
